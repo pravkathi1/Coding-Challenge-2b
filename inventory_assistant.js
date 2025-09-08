c41596b5b724940d53302fb732509ebac5449833
Pravallika <pkathi@usf.edu>
2025-09-07 23:16:12 -0400

    commit

--- inventory_assistant.js
+++ inventory_assistant.js
@@ -3,19 +3,19 @@
 let itemName = "Labubu";
 let unitCost = 50;
 let currentStock = 100;
-let reorderLevel = 70;
+let reorderLevel = 101;
 let targetStock = 110
 let weeklyDemand = 30
-let supplierLeadTimeWeeks = 1
+let supplierLeadTimeWeeks = 2
 
 weeksOfCover = weeklyDemand > 0 ? currentStock / weeklyDemand : Infinity
 stockDeficit = Math.max(0, targetStock - currentStock)
 reorderQuantity = (currentStock <= reorderLevel || weeksOfCover < supplierLeadTimeWeeks) ? Math.ceil(stockDeficit) : 0
 estimatedReorderCost = reorderQuantity * unitCost
-reorderNow (boolean) = currentStock <= reorderLevel || weeksOfCover < supplierLeadTimeWeeks
+let reorderNow = currentStock <= reorderLevel || weeksOfCover < supplierLeadTimeWeeks; // true or false
 
 console.log("Item Name:", + itemName)
 console.log("Weeks of Cover:", + weeksOfCover.toFixed(2))
-console.log("Reorder Now?", + reorderNow)
 console.log("Recommended Reorder Quantity:", + reorderQuantity)
 console.log("Estimated Reorder Cost:", + estimatedReorderCost.toFixed(2))
+console.log("Reorder Now?", reorderNow);
